6a16317ad5b37517e1072f5ed676f033b7ee7edd
Danil Schumin <danil80sch@gmail.com>
2023-07-28 21:28:04 +0200

    Pretty trashy get Champions in deck, still working tho

--- GameInfoManager.js
+++ GameInfoManager.js
@@ -1,10 +1,23 @@
-async function getScreen() {
-    const response = await fetch('http://127.0.0.1:21337/positional-rectangles').then(r => { return  r}).catch((e => {
-        console.log('Game State not available! Is the game running?')
+async function callLocalAPI(call) {
+    const response = await fetch('http://127.0.0.1:21337/' + call).then(r => {
+        return r
+    }).catch((e => {
+        console.log('Local Api not found! Is the game running?')
         throw new Error(e);
     }));
 
     return await response.json();
 }
 
-module.exports = { getScreen };
+async function callSetData(set) {
+    const response = await fetch('https://dd.b.pvp.net/latest/set' + set + '/en_us/data/set' + set + '-en_us.json').then(r => {
+        return r
+    }).catch((e => {
+        console.log('Dragon Data not found, are the Riot Servers down?')
+        throw new Error(e);
+    }));
+
+    return await response.json();
+}
+
+module.exports = { callLocalAPI, callSetData };
--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const DiscordRPC = require('discord-rpc');
 const GameInfo = require('./GameInfoManager');
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-const { clientId } = require('./config.json');
+const rpc = new DiscordRPC.Client({transport: 'ipc'});
+const {clientId} = require('./config.json');
+const {DeckEncoder} = require('runeterra');
 
-i = 0;
-async function setActivity(){
-    const GameState = await GameInfo.getScreen().then(r => {
-        switch (r['GameState']) {
-            case 'Menus': return 'In Menu';
-            case 'InProgress': return 'In Game';
-            default: return 'Unknown';
-        }
-    });
-    rpc.setActivity({
-        state: GameState,
-        largeImageKey: 'Trello',
-        largeImageText: 'Yeto',
-        smallImageKey: 'Verto',
-        startTimestamp: 0,
-    });
+async function setActivity() {
+    const GameState = await GameInfo.callLocalAPI('positional-rectangles').then(r => {return r['GameState']});
+    const GameResult = await GameInfo.callLocalAPI('game-result').then(r => {return r['GameID']});
+    let champions = await getDeckChampions('CIBQCAIBBIAQGCI5AECQCEICAUAQCAYJBURS4BIDBEKC2MBXI4CACAIBFUAQEAIBAECASDAIAMESCMZUHRFFAXDC');
+
+    switch (GameState) {
+        case 'Menus':
+            rpc.setActivity({
+                details: 'In Menu',
+                state: 'Games played: ' + (GameResult + 1),
+                largeImageKey: '01fr009',
+                smallImageKey: 'Verto',
+                startTimestamp: 0,
+            });
+            break;
+        case 'InProgress':
+            rpc.setActivity({
+                details: 'In Game',
+                state: 'Playing ',
+                largeImageKey: '01fr009',
+                smallImageKey: 'Verto',
+                startTimestamp: 0,
+            });
+            break;
+        default:
+            rpc.setActivity({
+                details: 'Unknown',
+                state: 'Games played: ' + (GameResult + 1),
+                largeImageKey: '01fr009',
+                smallImageKey: 'Verto',
+                startTimestamp: 0,
+            });
+    }
+
+}
+
+async function getDeckChampions(deckCode) {
+    let champions = [];
+    const deck = DeckEncoder.decode(deckCode);
+    for (let index = 0; index < deck.length; index++) {
+        const e = deck[index]
+        const t = await GameInfo.callSetData(e.code.slice(1, 2)).then(r => {
+            return r.filter(r => r.rarity === 'Champion' && r.cardCode === e.code)
+        });
+         champions.push(t);
+    }
+    return champions;
 }
 
 rpc.on('ready', () => {
@@ -29,4 +61,4 @@ rpc.on('ready', () => {
     }, 15e3);
 });
 
-rpc.login({ clientId: clientId }).catch(console.error);
+rpc.login({clientId: clientId}).catch(console.error);
