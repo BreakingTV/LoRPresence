e2bd6249e8fec996ada82952924fd57870fcfbb4
Danil Schumin <danil80sch@gmail.com>
2023-07-29 23:58:00 +0200

    get champions name + get if playing 'PoC' + add ToDos

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+* TODO: Better API calling
+* TODO: Clean Up code
+* TODO: Images for Game States
+* TODO: Better way of getting Champions
+*/
+
 const DiscordRPC = require('discord-rpc');
 const GameInfo = require('./GameInfoManager');
 const rpc = new DiscordRPC.Client({transport: 'ipc'});
@@ -6,36 +13,31 @@ const {DeckEncoder} = require('runeterra');
 
 async function setActivity() {
     const GameState = await GameInfo.callLocalAPI('positional-rectangles').then(r => {return r['GameState']});
-    const GameResult = await GameInfo.callLocalAPI('game-result').then(r => {return r['GameID']});
-    let champions = await getDeckChampions('CIBQCAIBBIAQGCI5AECQCEICAUAQCAYJBURS4BIDBEKC2MBXI4CACAIBFUAQEAIBAECASDAIAMESCMZUHRFFAXDC');
 
-    switch (GameState) {
-        case 'Menus':
-            rpc.setActivity({
-                details: 'In Menu',
-                state: 'Games played: ' + (GameResult + 1),
-                largeImageKey: '01fr009',
-                smallImageKey: 'Verto',
-                startTimestamp: 0,
-            });
-            break;
-        case 'InProgress':
-            rpc.setActivity({
-                details: 'In Game',
-                state: 'Playing ',
-                largeImageKey: '01fr009',
-                smallImageKey: 'Verto',
-                startTimestamp: 0,
-            });
-            break;
-        default:
-            rpc.setActivity({
-                details: 'Unknown',
-                state: 'Games played: ' + (GameResult + 1),
-                largeImageKey: '01fr009',
-                smallImageKey: 'Verto',
-                startTimestamp: 0,
-            });
+    if (GameState === 'InProgress') {
+        let GameMode;
+        let champions
+        if (await GameInfo.callLocalAPI('static-decklist').then(r => {return r['DeckCode']}) === null && GameInfo.callLocalAPI('static-decklist').then(r => {return r['CardsInDeck']}) !== null) {
+            GameMode = 'Path of Champions'
+            champions = 'WORK IN PROGRESS';
+        } else champions = await getDeckChampions(await GameInfo.callLocalAPI('static-decklist').then(r => {return r['DeckCode']}));
+
+
+        rpc.setActivity({
+            details: 'Playing: ' + GameMode,
+            state: 'Deck Champions: WORK IN PROGRESS',
+            largeImageKey: '01fr009',
+            smallImageKey: 'Verto',
+            startTimestamp: 0,
+        });
+    } else {
+        const GameResult = await GameInfo.callLocalAPI('game-result').then(r => {return r['GameID']});
+
+        rpc.setActivity({
+            details: 'In Menu',
+            state: 'Games played: ' + (GameResult + 1),
+            startTimestamp: 0,
+        });
     }
 
 }
@@ -45,10 +47,18 @@ async function getDeckChampions(deckCode) {
     const deck = DeckEncoder.decode(deckCode);
     for (let index = 0; index < deck.length; index++) {
         const e = deck[index]
-        const t = await GameInfo.callSetData(e.code.slice(1, 2)).then(r => {
-            return r.filter(r => r.rarity === 'Champion' && r.cardCode === e.code)
-        });
-         champions.push(t);
+        let set = e.code.slice(1, 2);
+
+        // TODO: Find a better solution, what is this???
+        let t = await GameInfo.callSetData(set).then(r => {return r.filter(r => r.rarity === 'Champion' && r.cardCode === e.code)});
+        if (set === '6' && t.length === 0) {
+            t = await GameInfo.callSetData(set + 'cde').then(r => {return r.filter(r => r.rarity === 'Champion' && r.cardCode === e.code)});
+        } else if (set === '7' && t.length === 0) {
+            t = await GameInfo.callSetData(set + 'b').then(r => {return r.filter(r => r.rarity === 'Champion' && r.cardCode === e.code)});
+        }
+        if (t.length !== 0) champions.push(t);
+
+
     }
     return champions;
 }
