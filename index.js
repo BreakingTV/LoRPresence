17de046ae55bd00ce927d94de46e3f5ccad583fa
Danil Schumin <danil80sch@gmail.com>
2023-08-02 01:25:08 +0200

    More code clean up + normals get champions works now

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
-/*
-* TODO: Better API calling
-* TODO: Clean Up code
-* TODO: Images for Game States
-* TODO: Better way of getting Champions
-*/
-
 const DiscordRPC = require('discord-rpc');
-const GameInfo = require('./GameInfoManager');
 const rpc = new DiscordRPC.Client({transport: 'ipc'});
 const {clientId} = require('./config.json');
 const {DeckEncoder} = require('runeterra');
-const {callLocalAPI} = require("./GameInfoManager");
 
 async function setActivity() {
-    const positionalRectangles =  await fetch('http://127.0.0.1:21337/positional-rectangles').then(r=> r.json() ).catch((e => { console.log('Local Api not found! Is the game running?'); throw new Error(e); }));
-    const staticDeckList = await fetch('http://127.0.0.1:21337/static-decklist').then(r=> r.json()).catch((e => { console.log('Local Api not found! Is the game running?'); throw new Error(e); }));
-
-    console.log(await getDeckChampions('CUCQCAYJEMAQICINAECQSCADAYFBUHZMAUDASEARDUPSGAYBAMERWAIGBEEACBQMAEAQCBQJF4'));
+    const positionalRectangles =  await fetch('http://127.0.0.1:21337/positional-rectangles').then(r=> r.json() );
+    const staticDeckList = await fetch('http://127.0.0.1:21337/static-decklist').then(r=> r.json());
+    const gameResult = await fetch('http://127.0.0.1:21337/game-result').then(r=> r.json());
 
     const GameState = positionalRectangles['GameState'];
+    let champions = await getChampions(staticDeckList['DeckCode']);
     if (GameState === 'InProgress') {
         let GameMode = 'Standard (Ranked)';
-        let champions = await getDeckChampions(staticDeckList['DeckCode']);
-        if (await staticDeckList['DeckCode'] === null && staticDeckList['CardsInDeck'] !== null) {
+        if (staticDeckList['DeckCode'] === null && staticDeckList['CardsInDeck'] !== null) {
             GameMode = 'Path of Champions';
-            champions = 'WORK IN PROGRESS';
+        } else {
+            champions = champions[0][0]['name'] + ' and ' + champions[1][0]['name'];
         }
-
         await rpc.setActivity({
             details: 'Playing: ' + GameMode,
-            state: 'Deck Champions: WORK IN PROGRESS',
+            state: 'Champions: ' + champions,
             largeImageKey: '01fr009',
             smallImageKey: 'Verto',
             startTimestamp: 0,
         });
     } else {
-        const GameResult = await GameInfo.callLocalAPI('game-result').then(r => {return r['GameID']});
+        const GameResult = await gameResult['GameID'];
 
         await rpc.setActivity({
             details: 'In Menu',
@@ -46,30 +36,22 @@ async function setActivity() {
 
 }
 
-async function getDeckChampions(deckCode) {
+async function getChampions(deckCode = null) {
     let filteredChampionData = [];
+    if (deckCode != null) {
     const deck = DeckEncoder.decode(deckCode);
-    for (let i = 0; i < deck.length; i++) {
-        let championData = await callSetData(6);
-        if (championData.length !== 0) filteredChampionData.push(championData);
+        for (let i = 0; i < deck.length; i++) {
+            let championData = await fetch('https://dd.b.pvp.net/latest/set' + deck[i].set + '/en_us/data/set' + deck[i].set + '-en_us.json').then(r => r.json().then(r => r.filter(r => r.rarity === 'Champion' && r.cardCode === deck[i].code)))
+            if (championData.length === 0) {
+                if (deck[i].set === 6) championData = await fetch('https://dd.b.pvp.net/latest/set' + deck[i].set + 'cde/en_us/data/set' + deck[i].set + 'cde-en_us.json').then(r => r.json().then(r => r.filter(r => r.rarity === 'Champion' && r.cardCode === deck[i].code)))
+                if (deck[i].set === 7) championData = await fetch('https://dd.b.pvp.net/latest/set' + deck[i].set + 'b/en_us/data/set' + deck[i].set + 'b-en_us.json').then(r => r.json().then(r => r.filter(r => r.rarity === 'Champion' && r.cardCode === deck[i].code)))
+            }
+            if (championData.length !== 0) filteredChampionData.push(championData);
+        }
+    } else {
+        filteredChampionData = 'WORK IN PROGRESS';
     }
-
     return filteredChampionData;
-}
-
-async function callSetData(set) {
-    let response = [];
-    let data = await fetch('https://dd.b.pvp.net/latest/set' + set + '/en_us/data/set' + set + '-en_us.json').then(r => r.json());
-    response.push(data[0]);
-
-    if (set === 6) {
-        let set6cde = await fetch('https://dd.b.pvp.net/latest/set6cde/en_us/data/set6cde-en_us.json').then(r => r.json());
-        response.push(set6cde[0]);
-    } else if (set === 7) {
-        let set7b = await fetch('https://dd.b.pvp.net/latest/set7b/en_us/data/set7b-en_us.json').then(r => r.json());
-        response.push(set7b[0]);
-    }
-    return response;
 }
 
 
@@ -81,6 +63,4 @@ rpc.on('ready', () => {
         setActivity();
     }, 15e3);
 });
-
-setActivity();
-//rpc.login({clientId: clientId}).catch(console.error);
+rpc.login({clientId: clientId}).catch(console.error);
