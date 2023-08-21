863472fee912bd7d70cc6a62733795dfbe2c7d2c
Danil Schumin <danil80sch@gmail.com>
2023-08-21 18:43:18 +0200

    Added check if the game is updated (not tested)

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const DiscordRPC = require('discord-rpc');
 const rpc = new DiscordRPC.Client({transport: 'ipc'});
-const {clientId} = require('./config.json');
+const {clientId, RGAPI, port} = require('./config.json');
 const {DeckEncoder} = require('runeterra');
 
+async function startup() {
+    let platformData = await fetch('https://europe.api.riotgames.com/lor/status/v1/platform-data?api_key=' + RGAPI).then(r => r.json())
+
+    if (fetch('http://127.0.0.1:' + port).then(r => r.ok)) console.log('Game is Running')
+    else console.log("Game isn't Running, check if the Game is running or you have the right port in the config");
+    
+    if (platformData['maintenances'].length === 0) console.log('No Maintenance found, continue')
+}
+
 async function setActivity() {
-    const positionalRectangles =  await fetch('http://127.0.0.1:21337/positional-rectangles').then(r=> r.json());
-    const staticDeckList = await fetch('http://127.0.0.1:21337/static-decklist').then(r=> r.json());
-    const gameResult = await fetch('http://127.0.0.1:21337/game-result').then(r=> r.json());
+    const positionalRectangles =  await fetch('http://127.0.0.1:' + port +'/positional-rectangles').then(r=> r.json());
+    const staticDeckList = await fetch('http://127.0.0.1:' + port + '/static-decklist').then(r=> r.json());
+    const gameResult = await fetch('http://127.0.0.1:' + port + '/game-result').then(r=> r.json());
 
     const GameState = positionalRectangles['GameState'];
     if (GameState === 'InProgress') {
@@ -20,6 +29,7 @@ async function setActivity() {
                 else championName = champion[0]['name'];
         }
     }
+
         await rpc.setActivity({
             details: 'Playing' + GameMode,
             state: 'Champions: ' + championName,
@@ -31,12 +41,11 @@ async function setActivity() {
             details: 'In Menu',
         });
     }
-
 }
 
 async function getChampions(deckCode = null) {
     let filteredChampionData = [];
-    const staticDeckList = await fetch('http://127.0.0.1:21337/static-decklist').then(r=> r.json());
+    const staticDeckList = await fetch('http://127.0.0.1:' + port + '/static-decklist').then(r=> r.json());
     for (let key of Object.keys(staticDeckList['CardsInDeck'])) {
         let set = key.substring(1,2);
         let championData = await fetch('https://dd.b.pvp.net/latest/set' + set + '/en_us/data/set' + set + '-en_us.json').then(r => r.json().then(r => r.filter(r => r.rarity === 'Champion' && r.cardCode === key)))
@@ -49,7 +58,6 @@ async function getChampions(deckCode = null) {
     return filteredChampionData;
 }
 
-
 rpc.on('ready', () => {
     setActivity();
 
@@ -57,4 +65,6 @@ rpc.on('ready', () => {
         setActivity();
     }, 15e3);
 });
+
+startup().then(r => console.log('Checked everything! Presence now running'));
 rpc.login({clientId: clientId}).catch(console.error);
