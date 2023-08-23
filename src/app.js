ef5f9881d0878c6c9391ecb582f5300cbf455e84
Danil Sch <danil80sch@gmail.com>
2023-08-23 20:49:58 +0200

    Added data to local ! ! NOT READY !!

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const DiscordRPC = require('discord-rpc');
 const rpc = new DiscordRPC.Client({transport: 'ipc'});
 const {clientId, port} = require('../config/config.json');
 const { setActivity } = require('./components/RPC.js');
+const { pushData } = require("./components/getChampions");
 
 rpc.on('ready', () => {
-    setActivity(rpc, port);
+    //setActivity(rpc, port);
+    pushData().then(r => console.log(r));
 
     setInterval(() => {
-        setActivity(rpc, port);
+        //setActivity(rpc, port);
     }, 15e3);
 });
 
--- src/components/getChampions.js
+++ src/components/getChampions.js
@@ -1,5 +1,7 @@
 /* TODO: Update Code, use cashed data */
+const fs = require('fs');
 
+/*
 async function getChampions(deckCode = null, port) {
     let filteredChampionData = [];
     const staticDeckList = await fetch('http://127.0.0.1:' + port + '/static-decklist').then(r=> r.json());
@@ -14,5 +16,33 @@ async function getChampions(deckCode = null, port) {
     }
     return filteredChampionData;
 }
+ */
 
-exports.getChampions = getChampions;
+async function pushData() {
+    let set = 0;
+    let setNotFound = false;
+    let data = [];
+
+    while (true) {
+        set++;
+        let championData = await fetch('https://dd.b.pvp.net/latest/set' + set + '/en_us/data/set' + set + '-en_us.json').then(async r => {
+            if (r.status !== 403) return r.json();
+            else setNotFound = true;
+        });
+
+
+        data.push(championData);
+        if (setNotFound) {
+            /* Exceptions, if a new sub set comes out, this code needs to be updated */
+            let set6cde = await fetch('https://dd.b.pvp.net/latest/set6cde/en_us/data/set6cde-en_us.json').then(async r => r.json());
+            let set7 = await fetch('https://dd.b.pvp.net/latest/set7b/en_us/data/set7b-en_us.json').then(async r => r.json());
+            data.push(set6cde, set7);
+            break;
+        }
+    }
+
+    /* TODO: Create Database and put the data in there */
+    fs.writeFile('data.json', JSON.stringify(data), 'utf8', (err) => err);
+}
+
+exports.pushData = pushData;
